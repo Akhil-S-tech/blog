c7c2664a64cbf6596eb2673e68444e679ef75512
a-khils <akhilsomanmeg@gmail.com>
2023-06-26 22:36:41 +0530

    user model created

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile,User
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['username','name','email']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=['username','name','email','avatar']
+  
--- user/models.py
+++ user/models.py
@@ -1,18 +1,82 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.urls import reverse
 
 # Create your models here.
-class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    name=models.CharField(max_length=20)
+
+# Custom user manager
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, name, password=None, **others):
+        if not email:
+            raise ValueError("Email is required")
+        if not username:
+            raise ValueError("Username is required")
+        if not name:
+            raise ValueError("Name is required")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, name=name, **others)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, name, password=None, **others):
+        others.setdefault("is_superuser", True)
+        others.setdefault("is_staff", True)
+        others.setdefault("is_active", True)
+
+        if others.get("is_superuser") is not True:
+            raise ValueError("Superuser is must be True")
+        if others.get("is_staff") is not True:
+            raise ValueError("Staff is must be True")
+        if others.get("is_active") is not True:
+            raise ValueError("Active is must be True")
+
+        return self.create_user(username, email, name, password, **others)
+
+
+# Custom user model
+
+class User(AbstractBaseUser,PermissionsMixin):
     username=models.CharField(max_length=20,unique=True)
+    name=models.CharField(max_length=20)
     email=models.EmailField(max_length=255,unique=True)
-    avatar=models.ImageField(upload_to="profile/",default="media/avatar.jpg")
-    bio=models.TextField(blank=True)
+    is_superuser=models.BooleanField(default=False)
+    is_staff=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "name"]
 
     def __str__(self):
         return self.username
 
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
 
 
+# base model inheritted by every model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    username = models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
+    email = models.CharField(max_length=255)
+    avatar = models.ImageField(default="default/avatar.jpg", upload_to="profile/")
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def get_absolute_url(self):
+        return reverse("profile", kwargs={"username": self.username})
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+        ordering = ["-created_at"]
