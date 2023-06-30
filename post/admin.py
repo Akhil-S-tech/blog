08dd5c1db9cd6453bbde86a44e713a85c93d3fda
Anirudh <anirudhani300@gmail.com>
2023-06-30 22:03:28 +0530

    models added into admin pannel

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Post, Category, Tag, Comment
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["author", "title", "category", "created_at"]
+    list_filter = ["author", "category", "created_at"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
+    list_filter = ["created_at"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
+    list_filter = ["created_at"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "post", "created_at"]
+    list_filter = ["user", "post", "created_at"]
