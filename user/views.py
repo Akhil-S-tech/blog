82b91ddd8bb1b99fa7fe61b4a2bdc984271a286c
Anirudh <anirudhani300@gmail.com>
2023-07-09 18:17:33 +0530

    forgot password email send function created

--- user/views.py
+++ user/views.py
@@ -8,12 +8,14 @@ from rest_framework.permissions import (
 )
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
+from .email import send_email_verification
 from .serializers import (
     UserSerializer,
     UserRegisterSerializer,
     UserLoginSerializer,
     ActivateSerializer,
     ChangePasswordSerializer,
+    ForgotPasswordSerializer,
 )
 from django.contrib.auth import get_user_model
 from .models import Profile
@@ -38,6 +40,8 @@ class UserViewSet(viewsets.ModelViewSet):
             self.permission_classes = [AllowAny]
         if self.action == "reset_password":
             self.permission_classes = [IsAuthenticated]
+        if self.action == "forgot_password":
+            self.permission_classes = [AllowAny]
         return super().get_permissions()
 
     def get_serializer_class(self):
@@ -51,6 +55,8 @@ class UserViewSet(viewsets.ModelViewSet):
             return ActivateSerializer
         if self.action == "reset_password":
             return ChangePasswordSerializer
+        if self.action == "forgot_password":
+            return ForgotPasswordSerializer
         return self.serializer_class
 
     def get_queryset(self):
@@ -116,4 +122,26 @@ class UserViewSet(viewsets.ModelViewSet):
         user.save()
         return Response(
             {"message": "password changes successfuly"}, status=status.HTTP_200_OK
+        )
+
+    @action(detail=False, methods=["POST"])
+    def forgot_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = USER.objects.filter(email=serializer.data.get("email")).first()
+
+        if user is None:
+            return Response(
+                {"message": "Email not fount"}, status=status.HTTP_404_NOT_FOUND
+            )
+        if user.is_verified == False:
+            return Response(
+                {"message": "Please verify your email"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        send_email_verification(user)
+        return Response(
+            {"message": f"Email send seccessfully {user.email}"},
+            status=status.HTTP_200_OK,
         )
