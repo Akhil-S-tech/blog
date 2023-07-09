6351be036403c65f452008f054c7311da5261daf
a-khils <akhilsomanmeg@gmail.com>
2023-07-09 14:21:22 +0530

    post crud function created

--- post/views.py
+++ post/views.py
@@ -1,60 +1,63 @@
 from rest_framework.response import Response
 from django.http import Http404
+from rest_framework import generics,status
+from .permissions import IsAuthor
+from rest_framework.permissions import IsAuthenticated
 
-from rest_framework.views import APIView
-from rest_framework import status
 
-from .serializers import BlogPostSerializer
+from .serializers import PostSerializer
 from .models import Post
 
-# List all posts
-class BlogView(APIView):
-    def get(self,request,format=None):
-        post= Post.objects.all()
-        serializer=BlogPostSerializer(post,many=True)
-        return Response(serializer.data)
 
-# Create a post     
-class BlogCreate(APIView):
-    def post(self, request, format=None):
-        serializer = BlogPostSerializer(data=request.data)
+
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    # permission_classes = [IsAdminUser]
+
+class PostRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_url_kwarg='post_id'
+
+    def retrieve(self, request, *args, **kwargs):
+
+        instance=self.get_object()
+        serializer=self.get_serializer(instance)
+        related_post=Post.objects.filter(tags__in=instance.tags.all()).exclude(id=instance.id)[:4]
+        data=serializer.data
+        data['related_post']=PostSerializer(related_post,many=True).data
+        return Response(data,status=status.HTTP_200_OK)
+
+class PostUpdateAPIView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def put(self, request, pk):
+        post = Post.objects.get(id=pk)
+        serializer = PostSerializer(post, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,
-                            status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+            serializer.save(user=request.user)
+            return Response({ "success": True, "message": "updated post" })
+        else:
+            # print(serializer.errors)
+            return Response({ "success": False, "message": "error updating post" })
+        
+class PostDeleteAPIView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_url_kwarg='post_id'
+    permission_classes=[IsAuthenticated,IsAuthor]
 
-# Details of a post 
-class BlogDetail(APIView):
-    def get_object(self, pk):
-        # Returns an object instance that should 
-        # be used for detail views.
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-  
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = BlogPostSerializer(post)
-        return Response(serializer.data)
     
-# Update a Post 
-class BlogUpdate(APIView):
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = BlogPostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-
- # delete a post         
-class BlogDelete(APIView):
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-  
+    # def destroy(self, request, *args, **kwargs):
+    #     try:
+    #         pk = kwargs.get("pk")
+    #         post = Post.objects.get(id=pk)
+    #         if post.user.id == request.user.id:
+    #             self.perform_destroy(post)
+    #             return Response({ "success": True, "message": "post deleted" })
+    #         else:
+    #             return Response({ "success": False, "message": "not enough permissions" })
+    #     except ObjectDoesNotExist:
+    #         return Response({ "success": False, "message": "post does not exist" })
