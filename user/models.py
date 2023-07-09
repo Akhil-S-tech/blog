fc74d8a2e35b7881847851e51f119ed45f581328
Anirudh <anirudhani300@gmail.com>
2023-07-09 16:08:17 +0530

    email verifi field added

--- user/models.py
+++ user/models.py
@@ -44,6 +44,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     is_superuser=models.BooleanField(default=False)
     is_staff=models.BooleanField(default=False)
     is_active=models.BooleanField(default=True)
+    is_verified=models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now=True)
     objects = UserManager()
     USERNAME_FIELD = "username"
