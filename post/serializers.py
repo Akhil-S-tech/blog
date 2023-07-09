c570fba2f4a2ed042b1ea46deb689c3e87f65c6f
a-khils <akhilsomanmeg@gmail.com>
2023-07-09 14:17:44 +0530

    created AuthorSerializers, PostSerializer and its functions

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 from .models import Post
+from user.models import Profile
 
 
-class BlogPostSerializer(serializers.ModelSerializer):
+class AuthorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields= ["id","name","username",'email','avatar']
+    
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author=AuthorSerializers(read_only=True)
+    category=serializers.CharField()
+    tags=serializers.ListSerializer(child=serializers.CharField())
     class Meta:
         model=Post
-        fields= '__all__'
+        fields= ["id","author","title",'image','body','category','tags','likes','created_at','updated_at']
+    def validate(self, attrs):
+        return super().validate(attrs)
+    def create(self, validated_data):
+        return super().create(validated_data)
+    
     
