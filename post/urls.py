8c4425173fa9e76747c17d3d53c6339c22978976
a-khils <akhilsomanmeg@gmail.com>
2023-07-09 14:22:45 +0530

    added post crud urls

--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+# from .views import PostListCreateAPIView,BlogView,BlogUpdate,BlogCreate,BlogDetail
 
+urlpatterns = [
+    path('', views.PostListCreateAPIView.as_view()),
+    path('<str:post_id>/', views.PostRetrieveAPIView.as_view()),
+    path('update/<str:post_id>/', views.PostUpdateAPIView.as_view()),
+    path('delete/<str:post_id>/', views.PostDeleteAPIView.as_view()),
 
-urlpatterns = []
+]
