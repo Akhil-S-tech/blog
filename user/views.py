1a24463419f276b024cfca124ca135357039cff5
Anirudh <anirudhani300@gmail.com>
2023-07-09 17:16:24 +0530

    created login view

--- user/views.py
+++ user/views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics, viewsets, status
 from rest_framework.decorators import action
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
     IsAuthenticatedOrReadOnly,
 )
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
-from .serializers import UserSerializer, UserRegisterSerializer
+from .serializers import UserSerializer, UserRegisterSerializer, UserLoginSerializer
 from django.contrib.auth import get_user_model
 from .models import Profile
 from django.db.models import Q
@@ -24,6 +26,8 @@ class UserViewSet(viewsets.ModelViewSet):
             self.permission_classes = [IsAuthenticatedOrReadOnly]
         if self.action == "register":
             self.permission_classes = [AllowAny]
+        if self.action == "login":
+            self.permission_classes = [AllowAny]
         return super().get_permissions()
 
     def get_serializer_class(self):
@@ -31,6 +35,8 @@ class UserViewSet(viewsets.ModelViewSet):
             return UserSerializer
         if self.action == "register":
             return UserRegisterSerializer
+        if self.action == "login":
+            return UserLoginSerializer
         return self.serializer_class
 
     def get_queryset(self):
@@ -48,3 +54,29 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=["POST"])
+    def login(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get("username")
+        password = serializer.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        if user.is_verified == False:
+            return Response(
+                {"error": "Please verify your email"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {"refresh": str(refresh), "access": str(refresh.access)},
+            status=status.HTTP_200_OK,
+        )
